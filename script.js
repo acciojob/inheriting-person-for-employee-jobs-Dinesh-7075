a4d45ff61bd83f4c07ca3893f4163d0cb93fcc88
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-07-18 08:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ function Person(name, age) {
 Person.prototype.greet = function(name, age) {
 	console.log(`Hello, my name is ${name}, I am ${age} years old.`);
 }
-const dineshDetails = new Person('Dinesh', 23);
-console.log(dineshDetails);
-dineshDetails.greet('Dinesh', 23);
+const person  = new Person("Alice", 25);
+console.log(person );
+person .greet("Alice", 25);
 
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
 	this.jobTitle = jobTitle;
 }
-Employee.prototype.jobGreet = function(name, age,jobTitle) {
+Employee.prototype.jobGreet = function(name, age, jobTitle) {
 	console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}`, );
 }
-const EmpDetails = new Employee('Dinesh', 25, 'SDE');
-console.log(EmpDetails);
-EmpDetails.jobGreet('Dinesh', 25, 'SDE');
+const employee  = new Employee("Bob", 30, "Manager");
+console.log(employee );
+employee .jobGreet("Bob", 30, "Manager");
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
